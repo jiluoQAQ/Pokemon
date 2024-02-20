898b638666a307fc7070c1d2bcc7a3cd77ac0f86
a376148946 <376148946@qq.com>
2024-02-20 14:48:03 +0800

    闪光图下载指向更新

--- Pokemon/utils/resource/download_from_cos.py
+++ Pokemon/utils/resource/download_from_cos.py
@@ -1,6 +1,6 @@
 from gsuid_core.utils.download_resource.download_core import download_all_file
 
-from .RESOURCE_PATH import CHAR_ICON_PATH
+from .RESOURCE_PATH import CHAR_ICON_PATH, CHAR_ICON_S_PATH
 
 
 async def check_use():
@@ -8,6 +8,7 @@ async def check_use():
         'Pokemon',
         {
             'resource/icon': CHAR_ICON_PATH,
+            'resource/staricon': CHAR_ICON_S_PATH,
         },
     )
     return 'pm全部资源下载完成!'
